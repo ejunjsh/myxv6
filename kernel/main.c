c27042934de419354b9ef0083e86fcc2a4b1e912
zhouff <sjj050121014@163.com>
2021-05-11 16:50:41 +0800

    finish main.c

--- kernel/main.c
+++ kernel/main.c
@@ -6,7 +6,7 @@
 
 volatile static int started = 0;
 
-// start() jumps here in supervisor mode on all CPUs.
+// 所有cpu从start() 跳到这里，这时候就是内核模式了 
 void
 main()
 {
@@ -16,19 +16,19 @@ main()
     printf("\n");
     printf("xv6 kernel is booting\n");
     printf("\n");
-    kinit();         // physical page allocator
-    kvminit();       // create kernel page table
-    kvminithart();   // turn on paging
-    procinit();      // process table
-    trapinit();      // trap vectors
-    trapinithart();  // install kernel trap vector
-    plicinit();      // set up interrupt controller
-    plicinithart();  // ask PLIC for device interrupts
-    binit();         // buffer cache
-    iinit();         // inode cache
-    fileinit();      // file table
-    virtio_disk_init(); // emulated hard disk
-    userinit();      // first user process
+    kinit();         // 物理页分配器
+    kvminit();       // 创建内核页表
+    kvminithart();   // 启用分页
+    procinit();      // 进程表
+    trapinit();      // 陷阱向量
+    trapinithart();  // 安装内核陷阱向量
+    plicinit();      // 设置中断控制器
+    plicinithart();  // 向PLIC配置设备中断
+    binit();         // 缓冲区缓存
+    iinit();         // inode缓存
+    fileinit();      // 文件表
+    virtio_disk_init(); // 模拟硬盘
+    userinit();      // 第一个用户进程
     __sync_synchronize();
     started = 1;
   } else {
@@ -36,9 +36,9 @@ main()
       ;
     __sync_synchronize();
     printf("hart %d starting\n", cpuid());
-    kvminithart();    // turn on paging
-    trapinithart();   // install kernel trap vector
-    plicinithart();   // ask PLIC for device interrupts
+    kvminithart();    // 启用分页
+    trapinithart();   // 安装内核陷阱向量
+    plicinithart();   // 向PLIC配置设备中断
   }
 
   scheduler();        
