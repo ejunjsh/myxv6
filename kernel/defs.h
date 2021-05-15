4bc8ff677f91b6aad9ecd34eb18a4c1e49ccf40b
zhouff <sjj050121014@163.com>
2021-05-15 22:50:14 +0800

    finish defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -183,5 +183,5 @@ void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
 void            virtio_disk_intr(void);
 
-// number of elements in fixed-size array
+// 返回固定大小的数组的元素个数
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
