c711a34e511d7cc8febcc75122b2e3672750fd44
zhouff <sjj050121014@163.com>
2021-05-15 23:28:49 +0800

    finish plic.c

--- kernel/plic.c
+++ kernel/plic.c
@@ -5,13 +5,13 @@
 #include "defs.h"
 
 //
-// the riscv Platform Level Interrupt Controller (PLIC).
+// riscv 平台级别中断控制器（PLIC）
 //
 
 void
 plicinit(void)
 {
-  // set desired IRQ priorities non-zero (otherwise disabled).
+  // 设置IRQ优先级为非零值，相当于启用中断（否则的话是禁用）
   *(uint32*)(PLIC + UART0_IRQ*4) = 1;
   *(uint32*)(PLIC + VIRTIO0_IRQ*4) = 1;
 }
@@ -20,15 +20,15 @@ void
 plicinithart(void)
 {
   int hart = cpuid();
-  
-  // set uart's enable bit for this hart's S-mode. 
+   
+  // 为当前cpu的S-mode下设置UART和VIRTIO0位
   *(uint32*)PLIC_SENABLE(hart)= (1 << UART0_IRQ) | (1 << VIRTIO0_IRQ);
 
-  // set this hart's S-mode priority threshold to 0.
+  // 设置当前cpu的S-mode的优先级阀值为0
   *(uint32*)PLIC_SPRIORITY(hart) = 0;
 }
 
-// ask the PLIC what interrupt we should serve.
+// 问PLIC发到当前cpu的中断是什么
 int
 plic_claim(void)
 {
@@ -37,7 +37,7 @@ plic_claim(void)
   return irq;
 }
 
-// tell the PLIC we've served this IRQ.
+// 告诉PLIC这个中断已经处理完
 void
 plic_complete(int irq)
 {
