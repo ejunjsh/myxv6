81fc204f9e030f9fde301fdbc49d8fa1c05d5580
zhouff <sjj050121014@163.com>
2021-05-19 19:03:21 +0800

    finish console.c

--- kernel/console.c
+++ kernel/console.c
@@ -1,12 +1,12 @@
 //
-// Console input and output, to the uart.
-// Reads are line at a time.
-// Implements special input characters:
-//   newline -- end of line
-//   control-h -- backspace
-//   control-u -- kill line
-//   control-d -- end of file
-//   control-p -- print process list
+// 控制台输入和输出，到uart。
+// 每次读取一行。
+// 实现特殊输入字符：
+//   newline -- 行尾
+//   control-h -- 退格
+//   control-u -- 清空一行
+//   control-d -- 文件结尾
+//   control-p -- 打印进程列表
 //
 
 #include <stdarg.h>
@@ -26,15 +26,15 @@
 #define C(x)  ((x)-'@')  // Control-x
 
 //
-// send one character to the uart.
-// called by printf, and to echo input characters,
-// but not from write().
+// 向uart发送一个字符。
+// 由printf调用，同时也用于回显输入字符，
+// 但不会被write()调用
 //
 void
 consputc(int c)
 {
   if(c == BACKSPACE){
-    // if the user typed backspace, overwrite with a space.
+    // 如果用户键入退格，请用空格覆盖
     uartputc_sync('\b'); uartputc_sync(' '); uartputc_sync('\b');
   } else {
     uartputc_sync(c);
@@ -44,16 +44,16 @@ consputc(int c)
 struct {
   struct spinlock lock;
   
-  // input
+  // 输入缓冲区
 #define INPUT_BUF 128
   char buf[INPUT_BUF];
-  uint r;  // Read index
-  uint w;  // Write index
-  uint e;  // Edit index
+  uint r;  // 读索引
+  uint w;  // 写索引
+  uint e;  // 修改索引
 } cons;
 
 //
-// user write()s to the console go here.
+// 用户调用write()写控制台会到这里
 //
 int
 consolewrite(int user_src, uint64 src, int n)
@@ -71,10 +71,10 @@ consolewrite(int user_src, uint64 src, int n)
 }
 
 //
-// user read()s from the console go here.
-// copy (up to) a whole input line to dst.
-// user_dist indicates whether dst is a user
-// or kernel address.
+// 用户调用read()读控制台会到这里
+// 将整个输入行复制到dst。
+// user_dist表示dst是否为用户
+// 或内核地址。
 //
 int
 consoleread(int user_dst, uint64 dst, int n)
@@ -86,8 +86,7 @@ consoleread(int user_dst, uint64 dst, int n)
   target = n;
   acquire(&cons.lock);
   while(n > 0){
-    // wait until interrupt handler has put some
-    // input into cons.buffer.
+    // 等待中断处理程序将一些输入放入cons缓冲区。
     while(cons.r == cons.w){
       if(myproc()->killed){
         release(&cons.lock);
@@ -98,16 +97,15 @@ consoleread(int user_dst, uint64 dst, int n)
 
     c = cons.buf[cons.r++ % INPUT_BUF];
 
-    if(c == C('D')){  // end-of-file
+    if(c == C('D')){  // 文件结束
       if(n < target){
-        // Save ^D for next time, to make sure
-        // caller gets a 0-byte result.
+        // 保存^D以备下次使用，以确保调用者获得0字节的结果。
         cons.r--;
       }
       break;
     }
 
-    // copy the input byte to the user-space buffer.
+    // 将输入字节复制到用户空间缓冲区
     cbuf = c;
     if(either_copyout(user_dst, dst, &cbuf, 1) == -1)
       break;
@@ -116,8 +114,7 @@ consoleread(int user_dst, uint64 dst, int n)
     --n;
 
     if(c == '\n'){
-      // a whole line has arrived, return to
-      // the user-level read().
+      // 整行已经到达，返回到用户级别read()。
       break;
     }
   }
@@ -127,10 +124,10 @@ consoleread(int user_dst, uint64 dst, int n)
 }
 
 //
-// the console input interrupt handler.
-// uartintr() calls this for input character.
-// do erase/kill processing, append to cons.buf,
-// wake up consoleread() if a whole line has arrived.
+// 控制台输入中断处理程序。
+// uartintr()为输入字符调用此函数。
+// 执行擦除/终止处理，附加到cons.buf，
+// 如果一整行都到了，唤醒consoleread()。
 //
 void
 consoleintr(int c)
@@ -138,17 +135,17 @@ consoleintr(int c)
   acquire(&cons.lock);
 
   switch(c){
-  case C('P'):  // Print process list.
+  case C('P'):  // 打印进程列表.
     procdump();
     break;
-  case C('U'):  // Kill line.
+  case C('U'):  // 删除一行.
     while(cons.e != cons.w &&
           cons.buf[(cons.e-1) % INPUT_BUF] != '\n'){
       cons.e--;
       consputc(BACKSPACE);
     }
     break;
-  case C('H'): // Backspace
+  case C('H'): // 退格
   case '\x7f':
     if(cons.e != cons.w){
       cons.e--;
@@ -159,15 +156,14 @@ consoleintr(int c)
     if(c != 0 && cons.e-cons.r < INPUT_BUF){
       c = (c == '\r') ? '\n' : c;
 
-      // echo back to the user.
+      // 回显给用户
       consputc(c);
 
-      // store for consumption by consoleread().
+      // 存储供consoleread()使用.
       cons.buf[cons.e++ % INPUT_BUF] = c;
 
       if(c == '\n' || c == C('D') || cons.e == cons.r+INPUT_BUF){
-        // wake up consoleread() if a whole line (or end-of-file)
-        // has arrived.
+       // 如果遇到换行符（或文件结尾），则唤醒consoleread()。
         cons.w = cons.e;
         wakeup(&cons.r);
       }
@@ -185,8 +181,7 @@ consoleinit(void)
 
   uartinit();
 
-  // connect read and write system calls
-  // to consoleread and consolewrite.
+  // 将读写系统调用连接到consoleread和consolewrite。
   devsw[CONSOLE].read = consoleread;
   devsw[CONSOLE].write = consolewrite;
 }
