9c04452c9517dadb16eb3fd1de60f3e118bdfcd7
zhouff <sjj050121014@163.com>
2021-06-04 00:16:02 +0800

    finish buf.h

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,11 +1,11 @@
 struct buf {
-  int valid;   // has data been read from disk?
-  int disk;    // does disk "own" buf?
+  int valid;   // 是否已从磁盘读取数据?
+  int disk;    // 磁盘“拥有”buf吗?
   uint dev;
   uint blockno;
   struct sleeplock lock;
   uint refcnt;
-  struct buf *prev; // LRU cache list
+  struct buf *prev; // LRU 缓存列表
   struct buf *next;
   uchar data[BSIZE];
 };
